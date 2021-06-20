c0d124fcee63500d93f12dc8e27d9cdb131bfae3
hossam-khalaf <hossamhassan303089@gmail.com>
2021-06-20 20:30:29 +0200

    basic modal strucure component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import Modal from './components/Modal'
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>click to open</h1>
 			<button className='open-modal-btn'>Open</button>
+			<Modal />
 		</div>
 	)
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Modal = () => {
+	return (
+		<div className='modal-overlay'>
+			<div className='modal-container'>
+				<button>X</button>
+				<div className='title'>
+					<h2>Sure, You want to continue?</h2>
+				</div>
+				<div className='body'>
+					<p>the next page is awesome! let's go there!</p>
+				</div>
+				<div className='footer'>
+					<button>Cancel</button>
+					<button>Continue</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Modal
